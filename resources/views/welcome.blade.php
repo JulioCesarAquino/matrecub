70e0740a2417a0dcdd9ad31908a9ba69c0a6743f
JulioCesarAquino <perfyu5@gmail.com>
2024-02-10 00:09:44 -0300

    Tag do google

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="nJVAAxvTtBLk5TvwX8iQlK6M24N529szemN6GJiarKs" />
 
         <title>Laravel</title>
 
